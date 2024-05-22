8d3e430d13f33f8b8e37ff8975e6d7fde6a4f730
Javier Gimenez <gimenez.javi.05@gmail.com>
2024-05-22 17:21:41 +0200

    updating fetures

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,13 @@ import 'react-toastify/dist/ReactToastify.css';
 function App() {
   return (
     <>
-      <div className="container mx-auto mt-20">
-        <h1 className="font-black text-5xl text-center md:w-2/3 md:mx-auto"
-        >Vet Patient <span className="text-indigo-700">Monitoring</span>
-        </h1>
+      <div className="container mx-auto mt-10">
+        <div className="flex gap-3 items-center justify-center border-b-4 border-blue-800 mx-5 pb-5">
+          <img src="pet-fingerprint.png" alt="fingerprint image" className="w-24" />
+          <h1 className="font-black text-5xl text-center"
+          >Veterinary <span className="text-indigo-700">Cares</span>
+          </h1>
+        </div>
         <div className="mt-12 md:flex">
           <Form />
           <PatientList />
